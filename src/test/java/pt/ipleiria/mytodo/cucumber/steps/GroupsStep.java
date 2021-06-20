905d130c65aeea658d3e152eadb7d844269769bb
David Cervantes Pérez <dacert@gmail.com>
2021-06-20 15:00:42 +0100

    conf for maven integration

--- src/test/java/pt/ipleiria/mytodo/cucumber/steps/GroupsStep.java
+++ src/test/java/pt/ipleiria/mytodo/cucumber/steps/GroupsStep.java
@@ -1,4 +1,4 @@
-package pt.ipleiria.mytodo.cucumber;
+package pt.ipleiria.mytodo.cucumber.steps;
 
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
@@ -11,61 +11,20 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import java.io.FileReader;
-import java.net.URL;
-import java.util.Iterator;
+import pt.ipleiria.mytodo.cucumber.ProjectParameters;
+import pt.ipleiria.mytodo.cucumber.Utils;
+
 import java.util.List;
-import java.util.Map;
 import static org.junit.Assert.assertTrue;
+
 
 public class GroupsStep {
     public AndroidDriver<AndroidElement> driver;
 
     @Before
     public void setUp() throws Exception {
-        JSONParser parser = new JSONParser();
-        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/pt/ipleiria/mytodo/cucumber/groups.conf.json"));
-        JSONArray envs = (JSONArray) config.get("environments");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        Map<String, String> envCapabilities = (Map<String, String>) envs.get(Integer.parseInt("1")); //selecting device index
-        Iterator it = envCapabilities.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-        }
-
-        Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
-        it = commonCapabilities.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            if(capabilities.getCapability(pair.getKey().toString()) == null){
-                capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-            }
-        }
-
-        String username = System.getenv("BROWSERSTACK_USERNAME");
-        if(username == null) {
-            username = (String) config.get("username");
-        }
-
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        if(accessKey == null) {
-            accessKey = (String) config.get("access_key");
-        }
-
-        String app = System.getenv("BROWSERSTACK_APP_ID");
-        if(app != null && !app.isEmpty()) {
-            capabilities.setCapability("app", app);
-        }
-
-        driver = new AndroidDriver(new URL("http://"+username+":"+accessKey+"@"+config.get("server")+"/wd/hub"), capabilities);
+        driver = Utils.setUp("src/test/resources/pt/ipleiria/mytodo/cucumber/groups.conf.json", 1);
     }
 
     @After
